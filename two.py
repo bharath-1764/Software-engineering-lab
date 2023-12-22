20084f27530c914ac49296efd1b5ab083a538338
bharath-1764 <97424743+bharath-1764@users.noreply.github.com>
2023-12-22 15:47:33 +0530

    two.py

--- two.py
+++ two.py
@@ -1,17 +1,24 @@
 import numpy as np
 
 def calculate_weather(wind, pressure, humidity):
-    # We use x and y for representing time and temperature
+    # Generate x values representing time
     x = np.linspace(-10, 10, 100)
-    y = wind * x**2 + pressure * x + humidity  # Quadratic equation written in terms of weather report
+    
+    # Quadratic equation written in terms of weather report
+    y = wind * x**2 + pressure * x + humidity  
+    
     return x, y
 
+# Coefficients for wind, pressure, and humidity
 wind = 9
 pressure = 7
 humidity = 2
 
+# Calculate weather data using the provided coefficients
 x_values, y_values = calculate_weather(wind, pressure, humidity)
 
+# Display the generated weather data for the single set of inputs
 print("Generated Weather Data for a Single Set of Inputs:")
 for i in range(len(x_values)):
     print(f"x={x_values[i]}, y={y_values[i]}")
+
